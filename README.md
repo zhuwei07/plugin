66a0b5767fa2c4d94634d8634b43e24ca18466cd
zhuwei07 <zhuwei07@mail.ustc.edu.cn>
2020-09-03 13:57:37 +0800

    修改脚本提示

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 cmder's plugin
 
 需要将setEnvAll.bat中的一行命令添加到../config/user_profile.cmd文件中
+注意修改amake下的amake脚本
--- amake/amake/amake.bat
+++ amake/amake/amake.bat
@@ -1,3 +1,3 @@
 @echo off
-set amakeroot=D:\tools\bin\amake
-nmake /NOLOGO -f %amakeroot%\windows\makefile.win prjfile=%1 amakeroot=%amakeroot%
+set "amakeroot=%~dp0"
+nmake /NOLOGO -f "%amakeroot%windows\makefile.win" prjfile=%1 amakeroot="%amakeroot%"
